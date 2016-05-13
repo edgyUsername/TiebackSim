1eabdd8bf8df2b6fbdf43c5ef75c5506133a1bcf
Mohamed Taguri <mohamed.taguri@gmail.com>
2016-05-13 17:25:49 +0100

    Start adding some physical parameters to sim data

---
+++ simulation/thermodynamics/PVT.py
@@ -0,0 +1,5 @@
+import sys
+from os import path
+sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+from data import SRKmixing as s
+# s.data is dict of SRK mixing parameters and MG for 13 gasses
