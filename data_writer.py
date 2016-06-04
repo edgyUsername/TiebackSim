070e6bdb1cbd2baed25914b24e204df9c26c9c44
Mohamed Taguri <mohamed.taguri@gmail.com>
2016-06-04 13:30:14 +0100

    Add data writer and create results folder

---
+++ data_writer.py
@@ -0,0 +1,10 @@
+import sys
+import csv
+from os import path
+sys.path.append(path.dirname( path.abspath(__file__)) ) 
+import settings
+
+def write_geometry_file(system):
+	pass
+def write_pressure_profile(system):
+	pass
